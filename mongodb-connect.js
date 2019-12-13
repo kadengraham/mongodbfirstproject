19bdeb2bcceffe7e7760c6140cb5ba8dbb9e3a93
Kaden Graham <kaden.graham@sixconsultingcorp.com>
2019-12-13 10:26:46 -0500

    first commit

---
+++ mongodb-connect.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+
+    const db = client.db('TestingFirstMongo');
+
+    if(err){
+        return console.log("Unable to connect to Mongodb server");
+    }
+
+    console.log("Connected to Mongodb server!!");
+
+    db.collection('TestingFirstMongo').insertOne({
+        taskName: 'Learning Mongodb',
+        completed: false
+    }, (err, result) => {
+        if(err){
+            console.log("There was an error inserting the new data");
+        }
+    })
+
+    client.close();
+
+})
